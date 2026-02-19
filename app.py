ce80d42cb8ce03fe47c6d60a422ad4a2746a7e7d
Katlan Santos <kkatlannatale01@gmail.com>
2026-02-19 15:27:33 -0300

    ajuste rota render

--- app.py
+++ app.py
@@ -385,5 +385,6 @@ def cadastrar_usuario():
         return jsonify({"msg": str(e)}), 500
 
 # ================= EXEC =================
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
